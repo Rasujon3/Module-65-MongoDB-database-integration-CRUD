89534cabf3faa67d00d62a9e287fd00f3e640f02
Rasujon3 <rasujon3@gmail.com>
2022-04-21 22:31:42 +0600

    65-7 Load and display data from database, create delete button

--- reat-node-mongodb/src/components/Home/Home.js
+++ reat-node-mongodb/src/components/Home/Home.js
@@ -1,9 +1,30 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 const Home = () => {
+  const [users, setUsers] = useState([]);
+  useEffect(() => {
+    const url = `http://localhost:5000/user`;
+    fetch(url)
+      .then((res) => res.json())
+      .then((data) => setUsers(data));
+  }, []);
+  const handleUserDelete = (id) => {
+    const proceed = window.confirm("Are u sure u want to delete?");
+    if (proceed) {
+      console.log("deleting user for id=", id);
+    }
+  };
   return (
     <div>
-      <h2>Home</h2>
+      <h2>Available users: {users.length} </h2>
+      <ul>
+        {users.map((user) => (
+          <li key={user._id}>
+            {user.name}::{user.email}
+            <button onClick={() => handleUserDelete(user._id)}>X</button>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 };
