8a8675905005495e6f5ccf38418a7606b7dfa778
Rasujon3 <rasujon3@gmail.com>
2022-04-21 21:00:10 +0600

    65-3 Send data to database using mongodb insertOne

--- node-mongo-crud/index.js
+++ node-mongo-crud/index.js
@@ -19,12 +19,20 @@ const client = new MongoClient(uri, {
   useUnifiedTopology: true,
   serverApi: ServerApiVersion.v1,
 });
-client.connect((err) => {
-  const collection = client.db("test").collection("devices");
-  console.log("DB Connected");
-  // perform actions on the collection object
-  client.close();
-});
+
+async function run() {
+  try {
+    await client.connect();
+    const userCollection = client.db("foodExpress").collection("users");
+    const user = { name: "Mohona Nodi", email: "nodi@gmail.com" };
+    const result = await userCollection.insertOne(user);
+    console.log(`User inserted with id: ${result.insertedId}`);
+  } finally {
+    // await client.close();
+  }
+}
+
+run().catch(console.dir);
 
 app.get("/", (req, res) => {
   res.send("Running My Nose CRUD Server");
